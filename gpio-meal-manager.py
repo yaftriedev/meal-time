3bbd23e1320f2728e4401d81b5eb65b8923f0cb3
yaftriedev <yaftrie@gmail.com>
2026-03-09 16:58:21 +0100

    Refactor LED control and add servo movement function
    Comment out LED control and add move_angle function for servo.

--- gpio-meal-manager.py
+++ gpio-meal-manager.py
@@ -4,27 +4,34 @@ from datetime import datetime
 from gpiozero import LED, Servo
 from config import gpio_pin_led, gpio_pin_servo, dispending_time, aditional_led_time, log_file
 
-led = LED(gpio_pin_led)
+# led = LED(gpio_pin_led)
 servo = Servo(gpio_pin_servo)
 
 hours = get_meal_time_array()
 actual_day = datetime.now().date()
+
+# Function to move Servo
+def move_angle(angle):
+    # limitar ángulo entre 0 y 180
+    angle = max(0, min(180, angle))
+    valor = (angle / 90) - 1
+    servo.value = valor
+    time.sleep(0.5)
 
 # Function to dispend
 def dispend(h):
     with open(log_file, "a") as l:
         l.write(f"Dispensing meal at {h} on {datetime.now()}\n")
 
-    servo.max()
-    led.on()
+    move_angle(0)
+    # led.on()
 
     time.sleep(dispending_time)
 
-    servo.min()
+    move_angle(45)
 
     time.sleep(aditional_led_time)
-
-    led.off()
+    # led.off()
 
 # min -> closed
 # max -> open
@@ -34,7 +41,7 @@ for i in range(1, 5):
     led.toggle()
     time.sleep(1)
 
-led.off()
+# led.off()
 
 while True:
     now = datetime.now()
