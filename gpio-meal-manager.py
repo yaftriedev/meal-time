cd6068cebb29c98242d2a2ca81ec2df6f96a80c1
yaftriedev <yaftrie@gmail.com>
2026-03-10 20:09:51 +0100

    Update gpio-meal-manager.py

--- gpio-meal-manager.py
+++ gpio-meal-manager.py
@@ -1,47 +1,27 @@
 import time
 from web.util import get_meal_time_array
 from datetime import datetime
-from gpiozero import LED, Servo
-from config import gpio_pin_led, gpio_pin_servo, dispending_time, aditional_led_time, log_file
+from gpiozero import Servo
+from config import *
 
-# led = LED(gpio_pin_led)
-servo = Servo(gpio_pin_servo)
+factory = PiGPIOFactory()
+servo = Servo(gpio_pin_servo, pin_factory=factory)
 
 hours = get_meal_time_array()
 actual_day = datetime.now().date()
-
-# Function to move Servo
-def move_angle(angle):
-    # limitar ángulo entre 0 y 180
-    angle = max(0, min(180, angle))
-    valor = (angle / 90) - 1
-    servo.value = valor
-    time.sleep(0.5)
 
 # Function to dispend
 def dispend(h):
     with open(log_file, "a") as l:
         l.write(f"Dispensing meal at {h} on {datetime.now()}\n")
 
-    move_angle(0)
-    # led.on()
+    servo.value = servo_min_angle
 
     time.sleep(dispending_time)
 
-    move_angle(45)
+    servo.value = servo_max_angle
 
-    time.sleep(aditional_led_time)
-    # led.off()
-
-# min -> closed
-# max -> open
-servo.min()
-
-for i in range(1, 5):
-    led.toggle()
-    time.sleep(1)
-
-# led.off()
+servo.value = servo_min_angle
 
 while True:
     now = datetime.now()
