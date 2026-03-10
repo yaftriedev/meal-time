81705f5f3727b00555dbe6d84e5355d382322934
yaftriedev <yaftrie@gmail.com>
2026-03-10 19:55:28 +0100

    Add pigpio installation to ngrok setup script
    Added installation steps for pigpio library after ngrok setup.

--- install-ngrok.sh
+++ install-ngrok.sh
@@ -4,3 +4,9 @@ curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
   | sudo tee /etc/apt/sources.list.d/ngrok.list \
   && sudo apt update \
   && sudo apt install ngrok
+
+wget https://github.com/joan2937/pigpio/archive/master.zip
+unzip master.zip
+cd pigpio-master
+make
+sudo make install
